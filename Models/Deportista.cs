3d48e35728381b618e4de2af776a7f2ba0ea49b5
LETICIA ROCÍO PASSER <48386947@ort>
2024-08-05 10:22:57 -0300

    clase deportista

---
+++ Models/Deportista.cs
@@ -0,0 +1,9 @@
+public class Pais{
+    private static int IdDeportista { get; set; }
+    private static string Nombre { get; set; }
+    private static string Apellido { get; set; }
+    private static DateTime FechaNacimiento { get; set; }
+    private static string Foto { get; set; }
+    private static int IdPais { get; set; }
+    private static int IdDeporte { get; set; }
+}
