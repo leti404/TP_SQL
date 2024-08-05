c7d6d80ff236d8143b79c4b8260b027189fe8bfd
LETICIA ROCÍO PASSER <48386947@ort>
2024-08-05 10:43:26 -0300

    clase bd

---
+++ Models/BD.cs
@@ -0,0 +1,36 @@
+public class BD{
+
+    public static void AgregarDeportista(int idDeportista)
+    {
+
+    }
+    public static void EliminarDeportista(int idDeportista)
+    {
+
+    }
+    public static void VerInfoDeporte(int idDeporte)
+    {
+
+    }
+    public static void VerInfoPais(int idPais)
+    {
+
+    }
+    public static void VerInfoDeportista(int idDeportista)
+    {
+
+    }
+    public static void ListarPaises()
+    {
+
+    }
+    public static void ListarDeportistasPorDeporte(int idDeporte)
+    {
+
+    }
+    public static void ListarDeportistasPorPais(int idPais)
+    {
+
+    }
+}
+
